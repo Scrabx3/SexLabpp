302588de58d2ea48d351b3120fccac764af49c9e
Scrabx3 <kris.vlntn@gmail.com>
2023-06-10 11:00:56 +0200

    add some new game forms

--- src/GameForms.h
+++ src/GameForms.h
@@ -14,21 +14,25 @@ namespace GameForms
 
 	// Vanilla Forms
 	inline RE::BGSKeyword* Vampire;
+	inline RE::BGSKeyword* ActorTypeNPC;
 	inline RE::BGSKeyword* DLC2RieklingMountedKeyword;
 
 	// SexLab Forms
 	inline const RE::TESFaction* GenderFaction;
 	inline const RE::TESFaction* AnimatingFaction;
+	inline const RE::TESFaction* ForbiddenFaction;
 
 	inline const RE::TESQuest* ConfigQuest;
 
 	inline bool LoadData()
 	{
 		LOOKUPGAME(Vampire, 0xA82BB);
+		LOOKUPGAME(ActorTypeNPC, 0x13794);
 		LOOKUPGAME(DLC2RieklingMountedKeyword, 0x0403A159);
 
 		LOOKUPMOD(GenderFaction, 0x043A43, "SexLab.esm");
 		LOOKUPMOD(AnimatingFaction, 0xE50F, "SexLab.esm");
+		LOOKUPMOD(ForbiddenFaction, 0x049068, "SexLab.esm");
 
 		LOOKUPMOD(ConfigQuest, 0xD62, "SexLab.esm");
 
