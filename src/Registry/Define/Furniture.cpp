f1039fd5362e7b5eece0bee52dd8ca68f505bfb4
Scrabx3 <kris.vlntn@gmail.com>
2023-06-15 09:23:24 +0200

    GetBedsInArea

--- src/Registry/Define/Furniture.cpp
+++ src/Registry/Define/Furniture.cpp
@@ -2,6 +2,50 @@
 
 namespace Registry
 {
+	std::vector<RE::TESObjectREFR*> BedHandler::GetBedsInArea(RE::TESObjectREFR* a_center, float a_radius, float a_radiusz)
+	{
+		const auto center = a_center->GetPosition();
+		std::vector<RE::TESObjectREFR*> ret{};
+		const auto add = [&](RE::TESObjectREFR& ref) {
+			if (!ref.GetBaseObject()->Is(RE::FormType::Furniture) && a_radiusz > 0.0f ? (std::fabs(center.z - ref.GetPosition().z) <= a_radiusz) : true)
+				if (Registry::BedHandler::IsBed(&ref))
+					ret.push_back(&ref);
+			return RE::BSContainer::ForEachResult::kContinue;
+		};
+		const auto TES = RE::TES::GetSingleton();
+		if (const auto interior = TES->interiorCell; interior) {
+			interior->ForEachReferenceInRange(center, a_radius, add);
+		} else if (const auto grids = TES->gridCells; grids) {
+			// Derived from: https://github.com/powerof3/PapyrusExtenderSSE
+			auto gridLength = grids->length;
+			if (gridLength > 0) {
+				float yPlus = center.y + a_radius;
+				float yMinus = center.y - a_radius;
+				float xPlus = center.x + a_radius;
+				float xMinus = center.x - a_radius;
+				for (uint32_t x = 0, y = 0; (x < gridLength && y < gridLength); x++, y++) {
+					const auto gridcell = grids->GetCell(x, y);
+					if (gridcell && gridcell->IsAttached()) {
+						auto cellCoords = gridcell->GetCoordinates();
+						if (!cellCoords)
+							continue;
+						float worldX = cellCoords->worldX;
+						float worldY = cellCoords->worldY;
+						if (worldX < xPlus && (worldX + 4096.0) > xMinus && worldY < yPlus && (worldY + 4096.0) > yMinus) {
+							gridcell->ForEachReferenceInRange(center, a_radius, add);
+						}
+					}
+				}
+			}
+			if (!ret.empty()) {
+				std::sort(ret.begin(), ret.end(), [&](RE::TESObjectREFR* a_refA, RE::TESObjectREFR* a_refB) {
+					return center.GetDistance(a_refA->GetPosition()) < center.GetDistance(a_refB->GetPosition());
+				});
+			}
+		}
+		return ret;
+	}
+
 	BedHandler::BedType BedHandler::GetBedType(const RE::TESObjectREFR* a_reference)
 	{
 		if (a_reference->GetName()[0] == '\0')
--- src/Registry/Define/Furniture.h
+++ src/Registry/Define/Furniture.h
@@ -12,6 +12,7 @@ namespace Registry
 			Double = 3,
 		};
 
+		_NODISCARD static std::vector<RE::TESObjectREFR*> GetBedsInArea(RE::TESObjectREFR* a_center, float a_radius, float a_radiusz);
 		_NODISCARD static BedType GetBedType(const RE::TESObjectREFR* a_reference);
 		_NODISCARD static bool IsBed(const RE::TESObjectREFR* a_reference);
 	};
