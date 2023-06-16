487ddac230e9aaa263f4d7ad0defa7aac1107dd3
Scrabx3 <kris.vlntn@gmail.com>
2023-06-16 18:38:34 +0200

    Remove define for the one defined in pch

--- src/Papyrus/sslActorLibrary.h
+++ src/Papyrus/sslActorLibrary.h
@@ -2,13 +2,11 @@
 
 namespace Papyrus::ActorLibrary
 {
-#define REGISTERFUNC(func, dont_delay) a_vm->RegisterFunction(#func##sv, "sslActorLibrary", func, dont_delay)
-
 	int32_t ValidateActor(VM* a_vm, StackID a_stackID, RE::TESQuest*, RE::Actor* a_actor);
 
 	inline bool Register(VM* a_vm)
 	{
-    REGISTERFUNC(ValidateActor, true);
+		REGISTERFUNC(ValidateActor, "sslActorLibrary", true);
 
 		return true;
   }
