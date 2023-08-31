2082e8c327f72f1cb921677600a85edf1b88a460
Scrabx3 <kris.vlntn@gmail.com>
2023-08-31 20:17:46 +0200

    sslActorAlias funcs

--- src/Papyrus/sslActorAlias.cpp
+++ src/Papyrus/sslActorAlias.cpp
@@ -1,5 +1,7 @@
 #include "sslActorAlias.h"
 
+#include "UserData/StripData.h"
+
 namespace Papyrus::sslActorAlias
 {
   // COMEBACK: Make a map to store charController flags?
@@ -70,20 +72,34 @@ namespace Papyrus::sslActorAlias
 		}
 	}
 
-	std::vector<RE::TESForm*> StripByData(VM* a_vm, StackID a_stackID, RE::BGSRefAlias* a_alias, Registry::Position::StripData a_stripdata, std::vector<uint32_t> a_overwrite)
+	std::vector<RE::TESForm*> StripByData(VM* a_vm, StackID a_stackID, RE::BGSRefAlias* a_alias,
+		Registry::Position::StripData a_stripdata, std::vector<uint32_t> a_defaults, std::vector<uint32_t> a_overwrite)
 	{
 		if (!a_alias) {
 			a_vm->TraceStack("Cannot call StripByData on a none alias", a_stackID);
 			return {};
 		}
-		return StripByDataEx(a_vm, a_stackID, a_alias, a_stripdata, a_overwrite, {});
+		return StripByDataEx(a_vm, a_stackID, a_alias, a_stripdata, a_defaults, a_overwrite, {});
 	}
 
 	std::vector<RE::TESForm*> StripByDataEx(VM* a_vm, StackID a_stackID, RE::BGSRefAlias* a_alias,
-		[[maybe_unused]] Registry::Position::StripData a_stripdata, 
-		std::vector<uint32_t> a_overwrite, 
-		std::vector<RE::TESForm*> a_mergewith)
+		Registry::Position::StripData a_stripdata,
+		std::vector<uint32_t> a_defaults,				// use if a_stripData == default
+		std::vector<uint32_t> a_overwrite,			// use if exists
+		std::vector<RE::TESForm*> a_mergewith)	// [HighHeelSpell, WeaponRight, WeaponLeft, Armor...]
 	{
+		using Strip = Registry::Position::StripData;
+		using SlotMask = RE::BIPED_MODEL::BipedObjectSlot;
+
+		enum MergeIDX {
+			Spell = 0,
+			Right = 1,
+			Left = 2,
+		};
+
+		if (a_mergewith.size() < 3) {
+			a_mergewith.resize(3, nullptr);
+		}
 		if (!a_alias) {
 			a_vm->TraceStack("Cannot call StripByDataEx on a none alias", a_stackID);
 			return a_mergewith;
@@ -93,31 +109,88 @@ namespace Papyrus::sslActorAlias
 			a_vm->TraceStack("ReferenceAlias must be filled with an actor reference", a_stackID);
 			return a_mergewith;
 		}
-		if (a_mergewith.size() < 3) {
-			a_mergewith.resize(3, nullptr);
+		uint32_t slots;
+		bool weapon;
+		if (a_overwrite[0] != 0) {
+			slots = a_overwrite[0];
+			weapon = a_overwrite[1];
+		} else if (a_stripdata == Strip::None) {
+			return a_mergewith;
+		} else if (a_stripdata == Strip::All) {
+			slots = static_cast<uint32_t>(-1);
+			weapon = true;
+		} else {
+			stl::enumeration<Strip, std::underlying_type<Strip>::type> stripnum;
+			if (stripnum.all(Strip::Default)) {
+				slots = a_defaults[0];
+				weapon = a_defaults[1];
+			} else {
+				slots = 0;
+				weapon = 0;
+			}
+			if (stripnum.all(Strip::Boots)) {
+				slots |= static_cast<uint32_t>(SlotMask::kFeet);
+			}
+			if (stripnum.all(Strip::Gloves)) {
+				slots |= static_cast<uint32_t>(SlotMask::kHands);
+			}
+			if (stripnum.all(Strip::Helmet)) {
+				slots |= static_cast<uint32_t>(SlotMask::kHead);
+			}
 		}
 
-		
-	// ; int[] Strip
-	// ; If(StripOverride.Length == 2)
-	// ; 	Strip = StripOverride
-	// ; Else
-	// ; 	Strip = Config.GetStripSettings(vanilla_sex == 1, Thread.UseLimitedStrip(), Thread.IsType[0], IsVictim())
-	// ; EndIf
-	// ; ; Gear
-	// ; If(Strip[1])
-	// ; 	RightHand = ActorRef.GetEquippedObject(1)
-	// ; 	If(RightHand && sslpp.CheckStrip(RightHand) != -1)
-	// ; 		ActorRef.UnequipItemEX(RightHand, ActorRef.EquipSlot_RightHand, false)
-	// ; 	EndIf
-	// ; 	LeftHand = ActorRef.GetEquippedObject(0)
-	// ; 	If(LeftHand && sslpp.CheckStrip(RightHand) != -1)
-	// ; 		ActorRef.UnequipItemEX(LeftHand, ActorRef.EquipSlot_LeftHand, false)
-	// ; 	EndIf
-	// ; EndIf
-	// ; Form[] gear = sslpp.StripActor(ActorRef, Strip[0])
-	// ; Equipment = PapyrusUtil.MergeFormArray(Equipment, gear)
-	// ; Log("STRIPPING -> Stripped Items: Weapon (Right):" + RightHand + " / Weapon (Left): " + LeftHand + " / Armor: " + Equipment)
+		if (weapon && actor->currentProcess) {
+			a_mergewith[Left] = actor->currentProcess->GetEquippedLeftHand();
+			a_mergewith[Right] = actor->currentProcess->GetEquippedRightHand();
+		}
+
+		const auto stripconfig = UserData::StripData::GetSingleton();
+		const auto manager = RE::ActorEquipManager::GetSingleton();
+		const auto& inventory = actor->GetInventory();
+		for (const auto& [form, data] : inventory) {
+			if (form->IsNot(RE::FormType::Armor) || !data.second->IsWorn()) {
+				continue;
+			}
+
+			switch (stripconfig->CheckStrip(form)) {
+			case UserData::Strip::NoStrip:
+				continue;
+			case UserData::Strip::None:
+				if (const auto biped = form->As<RE::TESObjectARMO>()) {
+					const auto slots = static_cast<uint32_t>(biped->GetSlotMask());
+					if ((slots & slots) == 0) {
+						continue;
+					// } else if (slots & static_cast<uint32_t>(SlotMask::kFeet)) {
+					// 	const auto hdtspell = [&]() -> RE::SpellItem* {
+					// 		static const auto* const heeleffect = RE::TESDataHandler::GetSingleton()->LookupForm<RE::EffectSetting>(0x800, "hdtHighHeel.esm"sv);
+					// 		if (!heeleffect)
+					// 			return nullptr;
+					// 		for (const auto& spell : actor->addedSpells) {
+					// 			if (!spell || spell->effects.empty())
+					// 				continue;
+					// 			for (const auto& effect : spell->effects) {
+					// 				if (effect && effect->baseEffect == heeleffect)
+					// 					return spell;
+					// 			}
+					// 		}
+					// 		return nullptr;
+					// 	}();
+					// 	if (hdtspell) {
+					// 		actor->RemoveSpell(hdtspell);
+					// 		a_mergewith[Spell] = hdtspell;
+					// 	}
+					}
+					a_mergewith.push_back(form);
+				}
+				break;
+			}
+			manager->UnequipObject(actor, form);
+		}
+    actor->Update3DModel();
+		return a_mergewith;
+	}
+
+  // NOTE: Stuff below isnt implemented (yet) wanna see if its actually needed
 	// ; ActorRef.QueueNiNodeUpdate()
 	// ; ; NiOverride High Heels
 	// ; If(Config.RemoveHeelEffect)
@@ -143,9 +216,6 @@ namespace Papyrus::sslActorAlias
 	// ; 		ActorRef.RemoveSpell(HDTHeelSpell)
 	// ; 	EndIf
 	// ; EndIf
-
-		return a_mergewith;
-	}
 
 }	 // namespace Papyrus::sslActorAlias
 
--- src/Papyrus/sslActorAlias.h
+++ src/Papyrus/sslActorAlias.h
@@ -1,14 +1,24 @@
 #pragma once
 
+#include "Registry/Animation.h"
+
 namespace Papyrus::sslActorAlias
 {
 	void LockActorImpl(VM* a_vm, StackID a_stackID, RE::BGSRefAlias* a_alias);
 	void UnlockActorImpl(VM* a_vm, StackID a_stackID, RE::BGSRefAlias* a_alias);
 
+	std::vector<RE::TESForm*> StripByData(VM* a_vm, StackID a_stackID, RE::BGSRefAlias* a_alias, 
+		Registry::Position::StripData a_stripdata, std::vector<uint32_t> a_defaults, std::vector<uint32_t> a_overwrite);
+	std::vector<RE::TESForm*> StripByDataEx(VM* a_vm, StackID a_stackID, RE::BGSRefAlias* a_alias,
+		Registry::Position::StripData a_stripdata, std::vector<uint32_t> a_defaults, std::vector<uint32_t> a_overwrite, std::vector<RE::TESForm*> a_mergewith);
+
 	inline bool Register(VM* a_vm)
 	{
 		REGISTERFUNC(LockActorImpl, "sslActorLibrary", false);
 		REGISTERFUNC(UnlockActorImpl, "sslActorLibrary", false);
+
+		REGISTERFUNC(StripByData, "sslActorLibrary", false);
+		REGISTERFUNC(StripByDataEx, "sslActorLibrary", false);
 
 		return true;
 	}
