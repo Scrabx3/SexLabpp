ab5be13654c6568ecd0eaedc2969f15e518f22df
Scrabx3 <kris.vlntn@gmail.com>
2023-09-17 11:02:19 +0200

    register funcs

--- src/Papyrus/sslActorLibrary.h
+++ src/Papyrus/sslActorLibrary.h
@@ -9,9 +9,22 @@ namespace Papyrus::ActorLibrary
 	void EraseStripAll(RE::StaticFunctionTag*);
 	int32_t CheckStrip(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::TESForm* a_form);
 
+	std::vector<RE::TESForm*> UnequipSlots(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::Actor* a_reference, uint32_t a_slotmask);
+
+	bool HasVehicle(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::Actor* a_actor);
+
 	inline bool Register(VM* a_vm)
 	{
 		REGISTERFUNC(ValidateActorImpl, "sslActorLibrary", true);
+
+		REGISTERFUNC(WriteStrip, "sslActorLibrary", true);
+		REGISTERFUNC(EraseStrip, "sslActorLibrary", true);
+		REGISTERFUNC(EraseStripAll, "sslActorLibrary", true);
+		REGISTERFUNC(CheckStrip, "sslActorLibrary", true);
+
+		REGISTERFUNC(UnequipSlots, "sslActorLibrary", false);
+
+		REGISTERFUNC(HasVehicle, "sslActorLibrary", true);
 
 		return true;
   }
