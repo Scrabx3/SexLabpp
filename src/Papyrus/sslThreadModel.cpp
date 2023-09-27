d988434a0edfb415278f781178eb67aad06c40c1
Scrabx3 <kris.vlntn@gmail.com>
2023-09-27 23:40:41 +0200

    new find center

--- src/Papyrus/sslThreadModel.cpp
+++ src/Papyrus/sslThreadModel.cpp
@@ -5,9 +5,8 @@
 #include "Registry/Library.h"
 #include "Registry/Util/CellCrawler.h"
 #include "Registry/Util/Scale.h"
-#include "UserData/ConfigData.h"
 
-using Offset = Registry::Offset;
+using Offset = Registry::CoordinateType;
 
 namespace Papyrus::ThreadModel
 {
@@ -39,166 +38,128 @@ namespace Papyrus::ThreadModel
 
 	RE::TESObjectREFR* FindCenter(VM* a_vm, StackID a_stackID, RE::TESQuest* a_qst,
 		const std::vector<RE::BSFixedString> a_scenes,
-		const RE::BSFixedString a_preferedscene,
-		RE::reference_array<RE::BSFixedString> a_out,
+		RE::reference_array<RE::BSFixedString> a_out_scenes,
+		RE::reference_array<float> a_out_coordinates,
 		FurniStatus a_status)
 	{
-		if (!a_qst) {
-			a_vm->TraceStack("Cannot call GetSetCenterObject on a none object", a_stackID);
-			return nullptr;
-		} else if (a_out.empty()) {
-			a_vm->TraceStack("Out array should have at least 1 place allocated", a_stackID);
+		if (a_scenes.empty()) {
+			a_vm->TraceStack("Missing scenes", a_stackID);
 			return nullptr;
 		}
-		std::vector<const Registry::Scene*> scenes_furni{};
-		std::vector<const Registry::Scene*> scenes_none{};
-		// get and sort scenes into furniture and non-furniure scenes
-		const auto sort_in_array = [&](auto scene) {
-			if (scene->furnitures.furnitures.get() != Registry::FurnitureType::None && a_status != FurniStatus::Disallow) {
-				scenes_furni.push_back(scene);
-			} else {
-				if (scene->furnitures.allowbed && a_status != FurniStatus::Disallow) {
-					scenes_furni.push_back(scene);
-				}
-				scenes_none.push_back(scene);
-			}
-		};
-		const auto lib = Registry::Library::GetSingleton();
-		const auto preferred_scene = lib->GetSceneByID(a_preferedscene);
-		for (auto&& sceneid : a_scenes) {
-			const auto scene = lib->GetSceneByID(sceneid);
-			if (!scene)
-				continue;
-			sort_in_array(scene);
-		}
-		if (scenes_furni.empty() && scenes_none.empty()) {
-			a_vm->TraceStack("Cannot set center object without any provided scenes", a_stackID);
+		if (a_out_scenes.empty()) {
+			a_vm->TraceStack("Result scene array must be allocated", a_stackID);
 			return nullptr;
 		}
-		// get aliases from quest,
-		// if center is given, check for an animation that uses center
-		// otherwise if preferred animation is given,
-		const auto get_compatible_type = [](const Registry::Scene* a_scene, Registry::FurnitureType a_type) -> bool {
-			switch (a_type) {
-			case Registry::FurnitureType::BedSingle:
-			case Registry::FurnitureType::BedDouble:
-				if (a_scene->furnitures.allowbed) {
-					return true;
-				}
-				__fallthrough;
-			default:
-				if (a_scene->furnitures.furnitures.all(a_type)) {
-					return true;
-				}
-				break;
-			}
-			return false;
-		};
+		if (a_out_coordinates.size() != 4) {
+			a_vm->TraceStack("Result coordinates must have length 4", a_stackID);
+			return nullptr;
+		}
 		const auto [center, actor] = GetAliasRefs(a_qst);
-		if (!actor) {
-			a_vm->TraceStack("Quest should have some actor positions filled", a_stackID);
+		if (!center && !actor) {
+			a_vm->TraceStack("Quest must have some actor positions filled", a_stackID);
 			return nullptr;
 		}
-		// is given center valid?
-		if (center) {
-			const auto centertype = Registry::FurnitureHandler::GetFurnitureType(center);
-			const auto scene = [&]() -> const Registry::Scene* {
-				if (preferred_scene && preferred_scene->IsCompatibleFurniture(centertype))
-					return preferred_scene;
-
-				if (centertype == Registry::FurnitureType::None)
-					return scenes_none.empty() ? nullptr : scenes_none[Random::draw<size_t>(0, scenes_none.size() - 1)];
-
-				for (auto&& scene : scenes_furni) {
-					if (get_compatible_type(scene, centertype)) {
-						return scene;
-					}
-				}
+		const auto library = Registry::Library::GetSingleton();
+		std::unordered_map<Registry::FurnitureType, std::vector<const Registry::Scene*>> scene_map{};
+		for (auto&& sceneid : a_scenes) {
+			const auto scene = library->GetSceneByID(sceneid);
+			if (!scene) {
+				a_vm->TraceStack("Invalid scene id in array", a_stackID);
 				return nullptr;
-			}();
-			if (scene) {
-				a_out[0] = scene->id;
-				return center;
 			}
-			return nullptr;
-		}
-		// non furniture animations
-		if (a_status == FurniStatus::Disallow) {
-			if (preferred_scene && preferred_scene->furnitures.furnitures == Registry::FurnitureType::None) {
-				a_out[0] = a_preferedscene;
-			} else {
-				if (scenes_none.empty()) {
-					return nullptr;
-				}
-				a_out[0] = scenes_none.at(Random::draw<size_t>(0, scenes_none.size() - 1))->id;
-			}
-			return actor;
-		}
-		// check cells for furniture compatible objects
-		std::vector<std::pair<RE::TESObjectREFR*, const Registry::Scene*>> potentials{};
-		const auto fromref = center ? center : actor;
-		RE::TESObjectREFR* retref = nullptr;
-		CellCrawler::ForEachObjectInRange(fromref, Settings::fScanRadius, [&](RE::TESObjectREFR& a_ref) {
-			// TODO: Check if the object is actually reachable from current location (or if theres a wall between them)
-			if (std::fabs(fromref->GetPositionZ() - a_ref.GetPosition().z) > 256)
-				return RE::BSContainer::ForEachResult::kContinue;	 // not same floor
-
-			// Skip, using actor for non furni animations
-			const auto type = Registry::FurnitureHandler::GetFurnitureType(&a_ref);
-			if (type == Registry::FurnitureType::None)
-				return RE::BSContainer::ForEachResult::kContinue;
-
-			// is pref compatible?
-			if (preferred_scene && preferred_scene->furnitures.furnitures.all(type)) {
-				a_out[0] = a_preferedscene;
-				retref = &a_ref;
-				return RE::BSContainer::ForEachResult::kStop;
-			}
-
-			// potentials
-			for (auto&& scene : scenes_furni) {
-				if (!scene || !get_compatible_type(scene, type)) {
+			if (scene->UsesFurniture()) {
+				if (a_status == FurniStatus::Disallow) {
 					continue;
 				}
-				potentials.emplace_back(&a_ref, scene);
-				scene = nullptr;
+			} else if (a_status != FurniStatus::Disallow) {
+				if (scene->furnitures.allowbed) {
+					std::array bedtypes{
+						Registry::FurnitureType::BedSingle,
+						Registry::FurnitureType::BedDouble,
+						Registry::FurnitureType::BedRoll
+					};
+					for (auto&& type : bedtypes) {
+						auto& it = scene_map[type];
+						if (std::ranges::find(it, scene) == it.end()) {
+							it.push_back(scene);
+						}
+					}
+				}
 			}
-			return RE::BSContainer::ForEachResult::kContinue;
-		});
-		if (retref) {	 // valid furniture for preferred scene
-			return retref;
-		} else if (potentials.size() && (a_status == FurniStatus::Prefer || Random::draw<int>(0, Settings::iFurniturePrefWeight))) {
-			const auto frompos = fromref->GetPosition();
-			std::sort(potentials.begin(), potentials.end(), [&](auto& a, auto& b) {
-				return a.first->GetPosition().GetDistance(frompos) < b.first->GetPosition().GetDistance(frompos);
-			});
-			a_out[0] = potentials[0].second->id;
-			return potentials[0].first;
-		} else {
-			if (scenes_none.empty()) {
+			auto& it = scene_map[scene->furnitures.furnitures.get()];
+			if (std::ranges::find(it, scene) == it.end()) {
+				it.push_back(scene);
+			}
+		}
+		assert(!scene_map.empty() && std::ranges::find_if(scene_map, [](auto& vec) { return vec.empty() } == scene_map.end()));
+		const auto ReturnData = [&](Registry::FurnitureType a_where, const Registry::Coordinate& a_coordinate) -> bool {
+			if (!scene_map.contains(a_where)) {
+				return false;
+			}
+			const auto& scenes = scene_map[a_where];
+			const auto count = std::min<size_t>(a_out_scenes.size(), scenes.size());
+			for (size_t i = 0; i < count; i++) {
+				a_out_scenes[i] = scenes[i]->id;
+			}
+			a_coordinate.ToContainer(a_out_coordinates);
+			return true;
+		};
+
+		if (center) {
+			const auto details = library->GetFurnitureDetails(center);
+			if (!details) {
+				Registry::Coordinate coord{ center };
+				return ReturnData(Registry::FurnitureType::None, coord) ? center : nullptr;
+			}
+			if (!details->HasType(scene_map, [](auto& it) { return it.first; })) {
 				return nullptr;
 			}
-			a_out[0] = scenes_none.at(Random::draw<size_t>(0, scenes_none.size() - 1))->id;
-			return actor;
+			const auto coords = details->GetClosestCoordinateInBound(center, actor);
+			if (coords.empty()) {
+				return nullptr;
+			}
+			const auto i = Random::draw<size_t>(0, coords.size() - 1);
+			return ReturnData(coords[i].first, coords[i].second) ? center : nullptr;
 		}
+		if (a_status == FurniStatus::Disallow || (a_status != FurniStatus::Prefer && !Random::draw<int>(0, Settings::iFurniturePrefWeight))) {
+			// no need to look for furniture if furnitures are disallowed or 1/PrefWight rng decides we dont use furniture
+			Registry::Coordinate coord{ actor };
+			return ReturnData(Registry::FurnitureType::None, coord) ? actor : nullptr;
+		}
+
+		std::vector<std::pair<RE::TESObjectREFR*, const Registry::FurnitureDetails*>> found_objects;
+		CellCrawler::ForEachObjectInRange(actor, Settings::fScanRadius, [&](RE::TESObjectREFR& a_ref) {
+			const auto details = library->GetFurnitureDetails(&a_ref);
+			if (!details || !details->HasType(scene_map, [](auto& it) { return it.first; })) {
+				return RE::BSContainer::ForEachResult::kContinue;
+			}
+			found_objects.push_back(std::make_pair(&a_ref, details));
+			return RE::BSContainer::ForEachResult::kContinue;
+		});
+		std::vector<std::tuple<Registry::FurnitureType, Registry::Coordinate, RE::TESObjectREFR*>> coords{};
+		for (auto&& [ref, details] : found_objects) {
+			const auto res = details->GetClosestCoordinateInBound(ref, actor);
+			for (auto&& pair : res) {
+				coords.push_back(std::make_tuple(pair.first, pair.second, ref));
+			}
+		}
+		if (!coords.empty()) {
+			std::sort(coords.begin(), coords.end(), [&](auto& a, auto& b) {
+				return std::get<1>(a).GetDistance(actor) < std::get<1>(b).GetDistance(actor);
+			});
+			const auto& res = coords[0];
+			return ReturnData(std::get<0>(res), std::get<1>(res)) ? std::get<2>(res) : nullptr;
+		}
+		Registry::Coordinate coord{ actor };
+		return ReturnData(Registry::FurnitureType::None, coord) ? actor : nullptr;
 	}
 
 	std::vector<float> GetBaseCoordinates(VM* a_vm, StackID a_stackID, RE::TESQuest* a_qst, RE::BSFixedString a_scene)
 	{
 		using FT = Registry::FurnitureType;
-
-		if (!a_qst) {
-			a_vm->TraceStack("Cannot call GetBaseCoordinates on a none object", a_stackID);
-			return { 0, 0, 0, 0 };
-		}
-
 		const auto [center, actor] = GetAliasRefs(a_qst);
 		if (!center) {
 			a_vm->TraceStack("Cannot get base coordinates from a none center", a_stackID);
-			return { 0, 0, 0, 0 };
-		}
-		if (!actor) {
-			a_vm->TraceStack("Missing some filled alias in quest", a_stackID);
 			return { 0, 0, 0, 0 };
 		}
 
@@ -209,18 +170,26 @@ namespace Papyrus::ThreadModel
 			center->GetAngleZ()
 		};
 
+		// const auto model = skyrim_cast<RE::TESModelTextureSwap*>(center);
+		// const auto transform = model ? UserData::FurnitureData::GetSingleton()->GetFurnitureTransform(model) : nullptr;
+		// if (transform) {
+
+
+		// 	return ret;
+		// }
+
 		// TODO: This here is a complete mess and nukes performance, w/e I was trying to do here aint worth it
 		switch (Registry::FurnitureHandler::GetFurnitureType(center)) {
 		case FT::None:
 			break;
 		case FT::BedDouble:
 		case FT::BedSingle:
-			// ret[Offset::Z] = 37.0
-			ret[Offset::Z] += 45;
+			// ret[Registry::CoordinateType::Z] = 37.0
+			ret[Registry::CoordinateType::Z] += 45;
 			break;
 		case FT::BedRoll:
-			ret[Offset::Z] += 7.5;
-			ret[Offset::R] += static_cast<float>(std::_Pi / 2);
+			ret[Registry::CoordinateType::Z] += 7.5;
+			ret[Registry::CoordinateType::R] += static_cast<float>(std::_Pi / 2);
 			break;
 			// default:
 			// 	const auto obj = center->Get3D();
@@ -261,30 +230,24 @@ namespace Papyrus::ThreadModel
 			// 	std::sort(neighbours.begin(), neighbours.end(), [&](auto a, auto b) {
 			// 		return a->offset.GetDistance(tmp) < b->offset.GetDistance(tmp);
 			// 	});
-			// 	ret[Offset::X] += neighbours[0]->offset[Offset::X];
-			// 	ret[Offset::Y] += neighbours[0]->offset[Offset::Y];
-			// 	ret[Offset::Z] += neighbours[0]->offset[Offset::Z];
-			// 	ret[Offset::R] += neighbours[0]->heading;
+			// 	ret[Registry::CoordinateType::X] += neighbours[0]->offset[Registry::CoordinateType::X];
+			// 	ret[Registry::CoordinateType::Y] += neighbours[0]->offset[Registry::CoordinateType::Y];
+			// 	ret[Registry::CoordinateType::Z] += neighbours[0]->offset[Registry::CoordinateType::Z];
+			// 	ret[Registry::CoordinateType::R] += neighbours[0]->heading;
 
 			// 	if (neighbours[0]->entryProperties.all(RE::BSFurnitureMarker::EntryProperties::kBehind)) {
-			// 		ret[Offset::R] += static_cast<float>(std::_Pi / 2);
+			// 		ret[Registry::CoordinateType::R] += static_cast<float>(std::_Pi / 2);
 			// 	}
 		}
-
-		// if (const auto base = center->GetBaseObject()) {
-		// 	if (const auto model = skyrim_cast<RE::TESModelTextureSwap*>(center)) {
-		// 		UserData::ConfigData::GetSingleton()->AdjustOffsetByModel(model, ret);
-		// 	}
-		// }
 
 		// const auto scene = Registry::Library::GetSingleton()->GetSceneByID(a_scene);
 		// if (!scene) {
 		// 	a_vm->TraceStack("Invalid scene id. Scene specific offset will NOT be applied", a_stackID);
 		// } else {
-		// 	ret[Offset::X] += scene->furnitures.offset[Offset::X];
-		// 	ret[Offset::Y] += scene->furnitures.offset[Offset::Y];
-		// 	ret[Offset::Z] += scene->furnitures.offset[Offset::Z];
-		// 	ret[Offset::R] += scene->furnitures.offset[Offset::R];
+		// 	ret[Registry::CoordinateType::X] += scene->furnitures.offset[Registry::CoordinateType::X];
+		// 	ret[Registry::CoordinateType::Y] += scene->furnitures.offset[Registry::CoordinateType::Y];
+		// 	ret[Registry::CoordinateType::Z] += scene->furnitures.offset[Registry::CoordinateType::Z];
+		// 	ret[Registry::CoordinateType::R] += scene->furnitures.offset[Registry::CoordinateType::R];
 		// }
 
 		return ret;
@@ -305,7 +268,7 @@ namespace Papyrus::ThreadModel
 		if (!scene) {
 			a_vm->TraceStack("Invalid scene id", a_stackID);
 			return "";
-		} else if (a_positions.size() > scene->positions.size()) {
+		} else if (a_positions.size() != scene->positions.size()) {
 			a_vm->TraceStack("Number positions do not match number of scene positions", a_stackID);
 			return "";
 		} else if (std::find(a_positions.begin(), a_positions.end(), nullptr) != a_positions.end()) {
@@ -321,26 +284,16 @@ namespace Papyrus::ThreadModel
 
 		for (size_t i = 0; i < a_positions.size(); i++) {
 			const auto& actor = a_positions[i];
-			std::array offset{
-				a_coordinates[Offset::X],
-				a_coordinates[Offset::Y],
-				a_coordinates[Offset::Z],
-				a_coordinates[Offset::R],
-			};
-			stage->positions[i].offset.Apply(offset);
+			Registry::Coordinate coordinate{ a_coordinates };
+			stage->positions[i].offset.Apply(coordinate);
 
-			actor->DoReset3D(false);
-			RE::NiPoint3 coordinate{
-				offset[Offset::X],
-				offset[Offset::Y],
-				offset[Offset::Z],
-			};
-			actor->data.angle.z = offset[Offset::R];
-			actor->SetPosition(coordinate, true);
+			actor->data.angle.z = coordinate.rotation;
+			actor->SetPosition(coordinate.AsNiPoint(), true);
 			Registry::Scale::GetSingleton()->SetScale(actor, scene->positions[i].scale);
 
 			// SKSE::GetTaskInterface()->AddTask([=]() {
-			actor->NotifyAnimationGraph(stage->positions[i].event);
+			const auto event = scene->GetNthAnimationEvent(stage, i);
+			actor->NotifyAnimationGraph(event);
 			// });
 		}
 
@@ -372,15 +325,10 @@ namespace Papyrus::ThreadModel
 			a_vm->TraceStack("Invalid stage id", a_stackID);
 			return;
 		}
-		std::array offset{
-			a_coordinates[Offset::X],
-			a_coordinates[Offset::Y],
-			a_coordinates[Offset::Z],
-			a_coordinates[Offset::R]
-		};
-		stage->positions[n].offset.Apply(offset);
-		a_position->data.angle.z = offset[Offset::R];
-		a_position->SetPosition({ offset[Offset::X], offset[Offset::Y], offset[Offset::Z] }, true);
+		Registry::Coordinate coordinate{ a_coordinates };
+		stage->positions[n].offset.Apply(coordinate);
+		a_position->data.angle.z = coordinate.rotation;
+		a_position->SetPosition(coordinate.AsNiPoint(), true);
 	}
 
 	bool GetIsCompatiblecenter(VM* a_vm, StackID a_stackID, RE::TESQuest*, RE::BSFixedString a_sceneid, RE::TESObjectREFR* a_center)
