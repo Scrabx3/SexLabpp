8a8082831b92adde95e018f5bdc68d9267b37464
Scrabx3 <kris.vlntn@gmail.com>
2023-11-24 10:35:23 +0100

    fix string split alg

--- src/Registry/Misc.cpp
+++ src/Registry/Misc.cpp
@@ -43,25 +43,30 @@ namespace Registry
 
 	std::vector<std::string_view> StringSplit(const std::string_view a_view, const char a_delim)
 	{
-		std::vector<std::string_view> result;
-
-		size_t previous = 0;
-		while (previous < a_view.size()) {
-			auto next = a_view.find(a_delim);
-
-			auto word = a_view.substr(previous, next);
+		const auto trim = [&](size_t from, size_t len) {
+			auto word = a_view.substr(from, len);
 			while (!word.empty() && std::isspace(word[0]))
 				word.remove_prefix(1);
 			while (!word.empty() && std::isspace(word.back()))
 				word.remove_suffix(1);
 
+			return word;
+		};
+		std::vector<std::string_view> result;
+		size_t previous = 0;
+		for (size_t i = 0; i < a_view.size(); i++) {
+			if (a_view[i] != a_delim)
+				continue;
+
+			auto word = trim(previous, i - previous);
 			if (!word.empty()) {
 				result.push_back(word);
 			}
-
-			previous = next;
+			previous = i + 1;
+		}
+		if (auto last = trim(previous, std::string_view::npos); !last.empty()) {
+			result.push_back(last);
 		}
 		return result;
 	}
-
 }
