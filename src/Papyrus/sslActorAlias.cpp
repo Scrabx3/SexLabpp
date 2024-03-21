0dd139cdfcd27e828258e6ee145885fdd231c7e0
ponzipyramid <ponzipyramid123@proton.me>
2024-03-21 16:35:38 -0400

    finished ng port (maybe)

--- src/Papyrus/sslActorAlias.cpp
+++ src/Papyrus/sslActorAlias.cpp
@@ -16,6 +16,8 @@ namespace Papyrus::ActorAlias
 			a_vm->TraceStack("LockActorImpl requires the filled reference to be an actor", a_stackID);
 			return;
 		}
+
+		const auto actorState = actor->AsActorState();
 		if (actor->IsPlayerRef()) {
 			RE::PlayerCharacter::GetSingleton()->SetAIDriven(true);
 
@@ -33,9 +35,9 @@ namespace Papyrus::ActorAlias
 				}
 			}
 
-			actor->actorState1.lifeState = RE::ACTOR_LIFE_STATE::kAlive;
+			actorState->actorState1.lifeState = RE::ACTOR_LIFE_STATE::kAlive;
 		} else {
-			actor->actorState1.lifeState = RE::ACTOR_LIFE_STATE::kRestrained;
+			actorState->actorState1.lifeState = RE::ACTOR_LIFE_STATE::kRestrained;
 		}
 
 		actor->StopCombat();
@@ -45,7 +47,7 @@ namespace Papyrus::ActorAlias
 		actor->StopInteractingQuick(true);
 		actor->SetCollision(false);
 
-		if (const auto process = actor->currentProcess) {
+		if (const auto process = actor->GetActorRuntimeData().currentProcess) {
 			process->ClearMuzzleFlashes();
 		}
 		actor->StopMoving(1.0f);
@@ -63,7 +65,7 @@ namespace Papyrus::ActorAlias
 			return;
 		}
 		Registry::Scale::GetSingleton()->RemoveScale(actor);
-		actor->actorState1.lifeState = actor->GetActorValue(RE::ActorValue::kHealth) <= 0 ? RE::ACTOR_LIFE_STATE::kDying : RE::ACTOR_LIFE_STATE::kAlive;
+		actor->AsActorState()->actorState1.lifeState = actor->AsActorValueOwner()->GetActorValue(RE::ActorValue::kHealth) <= 0 ? RE::ACTOR_LIFE_STATE::kDying : RE::ACTOR_LIFE_STATE::kAlive;
 		if (actor->IsPlayerRef()) {
 			RE::PlayerCharacter::GetSingleton()->SetAIDriven(false);
 		}
@@ -156,9 +158,12 @@ namespace Papyrus::ActorAlias
 				}
 			}
 		}
-		if (weapon && actor->currentProcess) {
-			a_mergewith[Left] = actor->currentProcess->GetEquippedLeftHand();
-			a_mergewith[Right] = actor->currentProcess->GetEquippedRightHand();
+
+		const auto currProcess = actor->GetActorRuntimeData().currentProcess;
+
+		if (weapon && currProcess) {
+			a_mergewith[Left] = currProcess->GetEquippedLeftHand();
+			a_mergewith[Right] = currProcess->GetEquippedRightHand();
 		}
 
 		const auto stripconfig = UserData::StripData::GetSingleton();
