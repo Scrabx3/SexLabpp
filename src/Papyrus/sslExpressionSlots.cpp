2ac36486dc929a3b548755462a0cbaf2c463e983
Scrabx3 <kris.vlntn@gmail.com>
2024-04-08 17:32:06 +0200

    impl exprslots papyrus interface

--- src/Papyrus/sslExpressionSlots.cpp
+++ src/Papyrus/sslExpressionSlots.cpp
@@ -14,4 +14,44 @@ namespace Papyrus::ExpressionSlots
     return ret;
 	}
   
+	std::vector<RE::BSFixedString> GetExpressionsByStatus(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::Actor* a_actor, int a_status)
+	{
+		std::vector<RE::BSFixedString> ret{};
+		RE::BSFixedString tag;
+		switch (a_status) {
+		case ExpressionStatus::Submissive:
+			tag = "Victim";
+			break;
+		case ExpressionStatus::Dominant:
+			tag = "Aggressor";
+			break;
+		default:
+			tag = "Normal";
+			break;
+		}
+		Registry::Expression::GetSingleton()->ForEachProfile([&](Registry::Expression::Profile& profile) {
+			if (!profile.tags.HasTag(tag)) {
+				return false;
+			}
+			ret.push_back(profile.id);
+			return false;
+		});
+		return ret;
+	}
+
+	std::vector<RE::BSFixedString> GetExpressionsByTags(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::Actor* a_actor, std::string_view a_tags)
+	{
+		std::vector<RE::BSFixedString> ret{};
+		auto list = Registry::StringSplit(a_tags);
+		Registry::TagDetails search{ list };
+		Registry::Expression::GetSingleton()->ForEachProfile([&](Registry::Expression::Profile& profile) {
+			if (!search.MatchTags(profile.tags)) {
+				return false;
+			}
+			ret.push_back(profile.id);
+			return false;
+		});
+		return ret;
+	}
+
 } // namespace Papyrus::ExpressionSlots
--- src/Papyrus/sslExpressionSlots.h
+++ src/Papyrus/sslExpressionSlots.h
@@ -2,11 +2,22 @@
 
 namespace Papyrus::ExpressionSlots
 {
-	std::vector<RE::BSFixedString> GetAllProfileIDs(RE::StaticFunctionTag*);
+	enum ExpressionStatus
+	{
+		None = 0,
+		Submissive = 1,
+		Dominant = 2
+	};
 
-	inline bool Register(VM* a_vm)
+	std::vector<RE::BSFixedString> GetAllProfileIDs(RE::StaticFunctionTag*);
+	std::vector<RE::BSFixedString> GetExpressionsByStatus(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::Actor* a_actor, int a_status);
+	std::vector<RE::BSFixedString> GetExpressionsByTags(VM* a_vm, StackID a_stackID, RE::StaticFunctionTag*, RE::Actor* a_actor, std::string_view a_tags);
+
+		inline bool Register(VM* a_vm)
 	{
 		REGISTERFUNC(GetAllProfileIDs, "sslExpressionSlots", true);
+		REGISTERFUNC(GetExpressionsByStatus, "sslExpressionSlots", true);
+		REGISTERFUNC(GetExpressionsByTags, "sslExpressionSlots", true);
 
 		return true;
 	}
