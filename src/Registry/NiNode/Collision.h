421b463d6357a2d87bf22bd3d0abe134b1aa4dc3
Scrabx3 <kris.vlntn@gmail.com>
2024-08-10 10:01:17 +0200

    refactor collisions

--- src/Registry/NiNode/Collision.h
+++ src/Registry/NiNode/Collision.h
@@ -2,120 +2,95 @@
 
 #include "Registry/Animation.h"
 #include "Registry/Define/Sex.h"
+#include "Registry/NiNode/Node.h"
 #include "Registry/Util/RayCast/ObjectBound.h"
 
-namespace Registry
+namespace Registry::Collision
 {
-	class Collision :
-		public Singleton<Collision>
+	constexpr inline auto INTERVAL = 128ms;
+
+	struct Position
 	{
-		struct TypeData
+		struct Interaction
 		{
-			enum class Type
+			enum class Action
 			{
-				None = -1,
-				VaginalP = 0,	 // being penetrated (passive)
-				AnalP = 1,
-				VaginalA = 2,	 // penetrating (active)
-				AnalA = 3,
-				Oral = 4,
-				Foot = 5,
-				Hand = 6,
-				Grinding = 7,
+				None = 0,
+				Vaginal,
+				Anal,
+
+				FootJob,
+				HandJob,
+				Grinding,
+
+				Cunnilingius,
+				Oral,
+				Deepthroat,
+				Skullfuck,
+				LickingShaft,
+
+				Kissing,
+				Facial,
 
 				Total,
 			};
 
-			RE::FormID _partner{ 0 };	 // actor performing type
-			Type _type{ Type::None };	 // action type performed by partner
-			float _velocity{ 0.0f };
-			float _distance{ 0.0f };
+			Interaction(RE::ActorPtr a_partner, Action a_action, float a_distance) :
+				partner(a_partner), action(a_action), distance(a_distance) {}
+			~Interaction() = default;
+
+			RE::ActorPtr partner{ 0 };
+			Action action{ Action::None };
+			float distance{ 0.0f };
+			float velocity{ 0.0f };
 		};
 
-		struct Position
+		struct Snapshot
 		{
-			struct Nodes
-			{
-				Nodes(const RE::Actor* a_actor, bool a_alternatenodes);
-				~Nodes() = default;
+			Snapshot(Position& a_position);
+			~Snapshot() = default;
 
-				RE::NiPointer<RE::NiNode> head;
-				RE::NiPointer<RE::NiAVObject> pelvis;
-				RE::NiPointer<RE::NiAVObject> spine_lower;
-
-				RE::NiPointer<RE::NiAVObject> hand_left;
-				RE::NiPointer<RE::NiAVObject> hand_right;
-				RE::NiPointer<RE::NiAVObject> foot_left;
-				RE::NiPointer<RE::NiAVObject> foot_rigt;
-
-				RE::NiPointer<RE::NiAVObject> clitoris;
-				RE::NiPointer<RE::NiAVObject> sos_base;
-				RE::NiPointer<RE::NiAVObject> sos_mid;
-				RE::NiPointer<RE::NiAVObject> sos_front;
-
-			public:
-				RE::NiPoint3 ApproximateTip() const;
-				RE::NiPoint3 ApproximateMid() const;
-				RE::NiPoint3 ApproximateBase() const;
-
-			private:
-				RE::NiPoint3 ApproximateNode(float a_forward, float a_upward) const;
-			};
+			std::vector<Interaction> GetHeadInteractions(const Snapshot& a_other) const;	// get interactions this actor is performing with its head
+			std::vector<Interaction> GetVaginalInteractions(const Snapshot& a_other) const;	// get interactions this actor is performing with its head
 
 		public:
-			Position(RE::Actor* a_owner, Sex a_sex);
-			~Position() = default;
-
-			Collision::TypeData* GetType(TypeData& a_data);
+			std::optional<RE::NiPoint3> GetHeadForwardPoint(float distance) const;
 
 		public:
-			RE::FormID _owner;
-			stl::enumeration<Sex> _sex;
-			Nodes _nodes;
-			std::vector<TypeData> _types;
+			Position& position;
+			ObjectBound bHead;
+
+			std::vector<Interaction> interactions{};
+			RE::NiPoint3 pGenitalReference{};
+			RE::NiPoint3 vCrotch{};
+			RE::NiPoint3 vSchlong{};
 		};
 
-		class PhysicsData
+	public:
+		Position(RE::Actor* a_owner, Sex a_sex) :
+			actor(a_owner), nodes(a_owner), sex(a_sex) {}
+		~Position() = default;
+
+	public:
+		RE::ActorPtr actor;
+		Node::NodeData nodes;
+		stl::enumeration<Sex> sex;
+		std::vector<Interaction> interactions{};
+	};
+
+	class Handler :
+		public Singleton<Handler>
+	{
+		class Process
 		{
-			struct WorkingData
-			{
-				WorkingData(Position& a_position);
-				~WorkingData() = default;
-
-				std::optional<TypeData> GetHeadInteraction(const WorkingData& a_partner) const; // get interactions this actor is performing with its head
-
-				std::optional<TypeData> GetsOral(const WorkingData& a_partner) const;				 // a_partner giving oral to this
-				std::optional<TypeData> GetsHandjob(const WorkingData& a_partner) const;		 // a_partner pleasuring this with hands
-				std::optional<TypeData> GetsFootjob(const WorkingData& a_partner) const;		 //  --- "" --- feet
-				std::optional<TypeData> DoesGrinidng(const WorkingData& a_partner) const;		 // a_partner penetrating this
-				std::optional<TypeData> HasIntercourse(const WorkingData& a_partner) const;	 // a_partner penetrating this
-
-			public:
-				float GetHeadForwardDistance() const { return bHead.boundMax.y; }
-				std::optional<RE::NiPoint3> GetHeadForwardPoint(float distance) const;
-
-			public:
-				Position& _position;
-			  ObjectBound bHead;
-				RE::NiNode* niGenitals;
-
-				std::vector<TypeData> types{};
-				RE::NiPoint3 pGenitalReference{};
-				RE::NiPoint3 vCrotch{};
-				RE::NiPoint3 vSchlong{};
-			};
-
 		public:
-			PhysicsData(std::vector<RE::Actor*> a_positions, const Scene* a_scene);
-			~PhysicsData();
-
-		public:
-			std::vector<Position> _positions;
+			Process(const std::vector<RE::Actor*>& a_positions, const Scene* a_scene);
+			~Process();
 
 		private:
 			void Update();
-
-			std::atomic<bool> _tactive;
+			std::vector<Position> positions;
+			std::atomic<bool> active;
 			std::thread _t;
 		};
 
@@ -123,10 +98,10 @@ namespace Registry
 		void Register(RE::FormID a_id, std::vector<RE::Actor*> a_positions, const Scene* a_scene) noexcept;
 		void Unregister(RE::FormID a_id) noexcept;
 		bool IsRegistered(RE::FormID a_id) const noexcept;
-		const PhysicsData* GetData(RE::FormID a_id) const;
+		const Process* GetProcess(RE::FormID a_id) const;
 
 	private:
-		std::vector<std::pair<RE::FormID, std::unique_ptr<PhysicsData>>> _data;
+		std::vector<std::pair<RE::FormID, std::unique_ptr<Process>>> processes;
 	};
 
 }	 // namespace Registry
