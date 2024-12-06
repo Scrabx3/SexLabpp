b410f633e2f49316aa9e3c9fa3e7c4b0f2eac3a9
Scrabx3 <kris.vlntn@gmail.com>
2024-12-06 16:30:41 +0100

    additional scene menu funcs

--- src/Papyrus/sslSceneMenu.h
+++ src/Papyrus/sslSceneMenu.h
@@ -4,21 +4,26 @@
 
 namespace Papyrus::SceneMenu
 {
-	inline void OpenMenu(RE::StaticFunctionTag*) { Registry::Interface::SceneMenu::Show(); }
-	inline void CloseMenu(RE::StaticFunctionTag*) { Registry::Interface::SceneMenu::Hide(); }
-	inline bool IsMenuOpen(RE::StaticFunctionTag*) { return Registry::Interface::SceneMenu::IsOpen(); }
+	void OpenMenu(RE::StaticFunctionTag*, RE::TESQuest* a_qst);
+	void CloseMenu(RE::StaticFunctionTag*, RE::TESQuest* a_qst);
+	bool IsMenuOpen(RE::StaticFunctionTag*);
 
-	void SetPositions(RE::StaticFunctionTag*, std::vector<RE::Actor*> a_positions);
-	void UpdateEnjoyment(RE::StaticFunctionTag*, RE::Actor* a_position, float a_enjoyment);
-	void SetEnjoyment(RE::StaticFunctionTag*, RE::Actor* a_position, float a_enjoyment);
+	void SetPositions(RE::StaticFunctionTag*, RE::TESQuest* a_qst, std::vector<RE::Actor*> a_positions);
+	void SetStage(STATICARGS, RE::TESQuest* a_qst, RE::BSFixedString a_sceneId, RE::BSFixedString a_stageId);
+	void SetTimer(RE::StaticFunctionTag*, RE::TESQuest* a_qst, float afTime);
+	void UpdateEnjoyment(RE::StaticFunctionTag*, RE::TESQuest* a_qst, RE::Actor* a_position, float a_enjoyment);
+	void SetEnjoyment(RE::StaticFunctionTag*, RE::TESQuest* a_qst, RE::Actor* a_position, float a_enjoyment);
 
 	inline bool Register(VM* a_vm)
 	{
     REGISTERFUNC(OpenMenu, "sslSceneMenu", true);
     REGISTERFUNC(CloseMenu, "sslSceneMenu", true);
     REGISTERFUNC(IsMenuOpen, "sslSceneMenu", true);
+    REGISTERFUNC(SetStage, "sslSceneMenu", true);
 
     REGISTERFUNC(SetPositions, "sslSceneMenu", true);
+    REGISTERFUNC(SetStage, "sslSceneMenu", true);
+    REGISTERFUNC(SetTimer, "sslSceneMenu", true);
     REGISTERFUNC(UpdateEnjoyment, "sslSceneMenu", true);
     REGISTERFUNC(SetEnjoyment, "sslSceneMenu", true);
 
