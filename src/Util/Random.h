900cf04e97aa38826239cbb4a898dd81e956109b
Scrabx3 <kris.vlntn@gmail.com>
2025-04-15 22:18:25 +0200

    Add draw function to draw random element from a container

--- src/Util/Random.h
+++ src/Util/Random.h
@@ -2,6 +2,14 @@
 
 #include <random>
 
+template <typename T>
+concept Container = requires(T a) {
+	typename T::value_type;
+	{ a.begin() } -> std::input_iterator;
+	{ a.end() } -> std::input_iterator;
+	{ a.size() } -> std::convertible_to<size_t>;
+};
+
 struct Random
 {
 	Random() = delete;
@@ -17,7 +25,17 @@ struct Random
 			return std::uniform_real_distribution<T>{ a_min, a_max }(eng);
 	}
 
-	template <class V>
+	template <Container T>
+	static inline typename T::value_type draw(const T& a_container)
+	{
+		if (a_container.empty()) {
+			throw std::out_of_range("Cannot draw from an empty container.");
+		}
+		std::uniform_int_distribution<size_t> dist{ 0, a_container.size() - 1 };
+		return *(a_container.begin() + dist(eng));
+	}
+
+	template <Container V>
 	static inline void shuffle(V& a_container)
 	{
 		std::ranges::shuffle(a_container, eng);
